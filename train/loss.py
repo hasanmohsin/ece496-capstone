e82f923cde63773c2ec672a6779d520c31c605d7
hasanmohsin <mohsin.hasan@mail.utoronto.ca>
2021-03-23 19:35:22 -0400

    Loss function batched

--- train/loss.py
+++ train/loss.py
@@ -18,14 +18,14 @@ def loss_RA_MIL(y, R, E, V):
         R:
             references
             -----------------------
-            The size of the matrix is M x M. M is the number of steps.
+            The size of the matrix is BxM x M. M is the number of (action) steps.
             
             R_lj contains a 1 if there is any backward reference between a_l and a_j.
             
         V:
             visual embeddings
             -----------------------
-            The size of the matrix is M x J x 768. M is the number of steps and J is the maximum
+            The size of the matrix is B x M x J x 768. M is the number of steps and J is the maximum
             number of entities across all of the steps.
             
             V_mj contains the visual embedding that corresponds to e_mj entity. This is the
@@ -35,48 +35,48 @@ def loss_RA_MIL(y, R, E, V):
         E:
             reference aware entity embeddings
             -----------------------
-            The size of the matrix is M x J x 768. E_mj contains the reference aware entity embedding
+            The size of the matrix is B x M x J x 768. E_mj contains the reference aware entity embedding
             of e_mj. Since each the steps may have a different number of entities.
     '''
-    
-    M = E.shape[0]
-    J = E.shape[1]
+    B = E.shape[0]
+    M = E.shape[1]
+    J = E.shape[2]
 
     # Compute the outer product between E and V. This essentially calculates
     # all possible alignment scores across the entire video. scores_lmjk is
     # the alignment score of e_mj and b_lk (same as equation). The size of
-    # the matrix is M x J x M x J.
+    # the matrix is B x M x J x M x J.
     #
     # Ref: stackoverflow.com/questions/24839481/python-matrix-outer-product
     #
     # Note that the alignment score for the padding matrices will be -inf due
     # to the way they're configured (1 * -inf).
-    scores = torch.einsum('mjd, lkd -> lmjk', E, V)
+    scores = torch.einsum('bmjd, blkd -> blmjk', E, V)
     #scores[scores.isnan()] = float('-inf')
 
     # The best alignment score between e_mj and b_lk (over all k).
     max_k_align = scores.max(3)[0]
     #max_k_align[max_k_align == float('-inf')] = 0
-
+    
     # Sum all of the best alignment scores from e_mj (over all j).
-    S_lm = max_k_align.sum(2)
+    S_lm = max_k_align.sum(3)
 
     # Find the transposed version.
-    S_ml = S_lm.transpose(0, 1)
+    S_ml = S_lm.transpose(1, 2)
 
     # Compute reference based penalty. 1 if none of the entities in e_m refer
     # to a_l, constant (hyperparameter) otherwise. We can use R since it has
     # the mappings between each of the actions. This is a M x M matrix.
-    Y_lm = torch.clip(R + y, 0, 1)
-    Y_ml = Y_lm.transpose(0, 1)
+    Y_lm = torch.clamp(R + y, 0.0, 1.0)
+    Y_ml = Y_lm.transpose(1, 2)
 
     # Zero matrix.
-    zero = torch.zeros(M, M)
+    zero = torch.zeros(B, M, M, dtype=torch.float)
 
     # S_ll needs to have the rows filled with diagonal values. Note that unsqueeze(1)
     # for a vector is the same as transposing it.
-    S_ll = S_lm.diagonal().unsqueeze(1).repeat(1, M)
-
+    S_ll = S_lm.diagonal(dim1=1, dim2=2).unsqueeze(2).repeat(1, 1, M)
+    
     # Vectorization magic.
     loss = (Y_lm * torch.max(zero, S_lm - S_ll) + Y_ml * torch.max(zero, S_ml - S_ll)).sum()
     
